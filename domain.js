a16e55da97e66766e61480ebeb697393ed68179d
Fraser-Greenlee <fraser.greenlee@icloud.com>
2016-04-28 20:16:48 +0100

    broke with brackets-builder-extended plugin

--- domain.js
+++ domain.js
@@ -4,7 +4,7 @@
     "use strict";
 
     var child_process = require("child_process"),
-        domainName = "builder.execute";
+        domainName = "idle.open";
 
     function exec(directory, command, callback) {
         child_process.exec(command, { cwd: directory}, function (err, stdout, stderr) {
